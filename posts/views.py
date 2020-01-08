00ea9b0d91c47b5233287735c4e4b143f6c709e4
moraes3000 <bruno_bmoraes@hotmail.com>
2020-01-08 20:37:00 -0300

    ajuste de template

--- posts/views.py
+++ posts/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic.list import ListView
 from django.views.generic.edit import UpdateView
 
 from comentarios.forms import FormComentario
 from comentarios.models import Comentario
 from .models import Post
-
+from django.contrib import messages
+from django.views import View
 # anotede
 from django.db.models import Q, Count, Case, When
 
@@ -94,4 +95,38 @@ class PostDetalhes(UpdateView):
         messages.success(self.request, 'Comentário enviado com sucesso.')
         return redirect('post_detalhes', pk=post.id)
 
+
+# class PostDetalhes(View):
+#     template_name = 'posts/post_detalhes.html'
+#
+#     def setup(self, request, *args, **kwargs):
+#         super().setup(request, *args, **kwargs)
+#
+#         pk = self.kwargs.get('pk')
+#         post = get_object_or_404(Post, pk=pk, publicado_post=True)
+#         self.contexto = {
+#             'post': post,
+#             'comentarios': Comentario.objects.filter(post_comentario=post,
+#                                                      publicado_comentario=True),
+#             'form': FormComentario(request.POST or None),
+#         }
+#
+#     def get(self, request, *args, **kwargs):
+#         return render(request, self.template_name, self.contexto)
+#
+#     def post(self, request, *args, **kwargs):
+#         form = self.contexto['form']
+#
+#         if not form.is_valid():
+#             return render(request, self.template_name, self.contexto)
+#
+#         comentario = form.save(commit=False)
+#
+#         if request.user.is_authenticated:
+#             comentario.usuario_comentario = request.user
+#
+#         comentario.post_comentario = self.contexto['post']
+#         comentario.save()
+#         messages.success(request, 'Seu comentário foi enviado para revisão.')
+#         return redirect('post_detalhes', pk=self.kwargs.get('pk'))
 
